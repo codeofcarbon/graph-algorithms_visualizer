53678b1fbf6ab5ab1546bfee6aaf5aedc7f2f0db
Krzysztof <code.of.carbon@gmail.com>
2021-12-21 20:59:19 +0100

    user can now connect nodes by edges, also switch mode between adding nodes and edges

---
+++ src/main/java/Edge.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Edge extends JComponent {
+    protected final JLabel edgeLabel;
+    protected final Vertex first;
+    protected final Vertex second;
+    protected int weight;
+
+    public Edge(Vertex first, Vertex second, int weight) {
+        this.first = first;
+        this.second = second;
+        this.weight = weight;
+        setName(String.format("Edge <%s -> %s>", first.id, second.id));
+        this.edgeLabel = new JLabel(String.valueOf(weight));
+        edgeLabel.setName(String.format("EdgeLabel <%s -> %s>", first.id, second.id));
+    }
+}
