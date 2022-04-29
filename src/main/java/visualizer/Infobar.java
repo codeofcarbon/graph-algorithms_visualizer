284ef7a761e34dece812f7a37933010fa0908cdc
Krzysztof <code.of.carbon@gmail.com>
2022-04-29 23:56:34 +0200

    added infobar class for handling showing results

---
+++ src/main/java/visualizer/Infobar.java
@@ -0,0 +1,34 @@
+package visualizer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Infobar extends JLabel {
+    private final Graph graph;
+
+    public Infobar(Graph graph) {
+        super("", SwingConstants.CENTER);
+        this.graph = graph;
+        setFont(new Font("Stylus BT", Font.PLAIN, 15));
+        setBackground(new Color(0, 0, 0, 0));
+        setForeground(new Color(204, 204, 204, 255));
+        graph.add(this);
+        graph.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent e) {
+                updateLocation();
+            }
+        });
+    }
+
+    void updateInfo(String info, String result) {
+        updateLocation();
+        setText(String.format("<html><div align='center'>%s",
+                info.isBlank() ? result : "<font color=#cccccc>" + info));
+    }
+
+    public void updateLocation() {
+        setSize(graph.getWidth(), 40);
+        setLocation(0, graph.getHeight() - 50);
+    }
+}
